3fcdae88efee4cd7d8c581a3de3217da7265f775
sangnt199tb <71488764+sangnt199tb@users.noreply.github.com>
2026-03-18 22:51:56 +0700

    BTVN quan ly thu vien

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -64,17 +64,23 @@ public class Main {
                     System.out.println("Số đầu sách có trong thư việc: " + count);
                     System.out.println("Danh sách các cuốn sách");
                     for (int i = 0; i < count; i ++){
-                        System.out.println("cuốn sách thứ " + i + titles[i] + " số lượng" + quantities[i]);
+                        System.out.println("cuốn sách thứ " + i + " " + titles[i] + " số lượng "  + quantities[i]);
                     }
                     break;
                 case 2:
+                    // kiểm tra số lượng sách đã vượt quá khả năng lưu trữ của thư viện hay chưa
+                    if(count >= 100){
+                        System.out.println("không thể nhập thêm sách vì đã vượt quá khả năng lưu trữ của thư viện");
+                    }
                     // Thêm sách
-                    System.out.println("Nhập vào tiêu đề sách");
+                    System.out.print("Nhập vào tiêu đề sách: ");
                     String newTitle = sc.nextLine();
-
-                    System.out.println("Nhập vào số lượng sách");
+                    System.out.print("Nhập vào số lượng sách: ");
                     int qty = sc.nextInt();
                     sc.nextLine();
+                    if(qty < 0){
+                        System.out.println("Số lượng sách nhập vào không được nhỏ hơn 0");
+                    }
 
                     titles[count] = newTitle;
                     quantities[count] = qty;
@@ -83,12 +89,75 @@ public class Main {
                     break;
                 case 3:
                     // Tìm sách
+                    String bookName;
+                    while (true) {
+                        System.out.print("Mời bạn nhập tên sách cần tìm: ");
+                        bookName = sc.nextLine();
+                        if (bookName.trim().length() == 0) {
+                            System.out.println("Tên sách không được bỏ trống!");
+                        } else {
+                            break;
+                        }
+                    }
+                    for (int i = 0; i < count; i++){
+                        if(titles[i].contains(bookName)){
+                            System.out.println(bookName + " còn " + quantities[i] + " quyển trong thư viện");
+                            break;
+                        }
+                    }
                     break;
                 case 4:
                     // Mượn sách
+                    String book;
+                    while (true) {
+                        System.out.print("Mời bạn nhập tên sách cần mượn: ");
+                        book = sc.nextLine();
+                        if (book.trim().isEmpty()) {
+                            System.out.println("Tên sách cần mượn không được bỏ trống!");
+                        } else {
+                            break;
+                        }
+                    }
+                    boolean found = false;
+                    for (int i = 0; i < count; i++) {
+                        if (titles[i].equalsIgnoreCase(book)) {
+                            found = true;
+                            if (quantities[i] == 0) {
+                                System.out.println("Cuốn sách \"" + book + "\" đã hết!");
+                            } else {
+                                quantities[i]--;
+                                System.out.println("Mượn sách thành công!");
+                                System.out.println("Còn lại " + quantities[i] + " quyển.");
+                            }
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        System.out.println("Cuốn sách bạn cần mượn không có trong thư viện");
+                    }
                     break;
                 case 5:
                     // Trả sách
+                    String theBooks;
+                    while (true) {
+                        System.out.print("Mời bạn nhập tên sách cần trả: ");
+                        theBooks = sc.nextLine();
+                        if (theBooks.trim().isEmpty()) {
+                            System.out.println("Tên sách cần cẩn trả không được bỏ trống!");
+                        } else {
+                            break;
+                        }
+                    }
+                    for (int i = 0; i < count; i++) {
+                        if (titles[i].equalsIgnoreCase(theBooks)) {
+                            found = true;
+                            quantities[i]++;
+                            System.out.println("Trả sách thành công!");
+                            System.out.println("Còn lại " + quantities[i] + " quyển.");
+                            break;
+                        }
+                    }
+                    System.out.println("Cuốn sách bạn cần mượn không có trong thư viện");
                     break;
                 case 6:
                     System.out.println("Exiting program...");
